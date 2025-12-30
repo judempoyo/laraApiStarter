dcaa27fb9011d0baaac67381d6832e2c788f26e7
Jude mpoyo <mpoyojude0@gmail.com>
2025-12-30 18:24:30 +0200

    feat : create register user action

---
+++ app/Actions/Auth/RegisterUserAction.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Actions\Auth;
+
+use App\DTOs\Auth\RegisterDTO;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterUserAction
+{
+    use \App\Traits\LogsActivity;
+
+    public function execute(RegisterDTO $dto): array
+    {
+        $user = User::create([
+            'name' => $dto->name,
+            'email' => $dto->email,
+            'password' => Hash::make($dto->password),
+        ]);
+
+        $this->logActivity('auth.register', "User {$user->email} registered.", $user->id);
+
+         if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail) {
+            $user->sendEmailVerificationNotification();
+        }
+
+        $token = $user->createToken('auth_token')->accessToken;
+
+
+        return [
+            'user' => $user,
+            'token' => $token,
+        ];
+    }
+}
