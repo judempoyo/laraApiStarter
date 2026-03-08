ac990bcccef686d5e2969d67af47d39b11b3d27b
Jude mpoyo <mpoyojude0@gmail.com>
2026-03-09 00:15:21 +0200

    fix: delete auth status  enums

--- app/Enums/Auth/ProfileStatus.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Enums\Auth;
-
-enum ProfileStatus: string
-{
-    case SUCCESS = 'SUCCESS';
-    case INVALID_CURRENT_PASSWORD = 'INVALID_CURRENT_PASSWORD';
-}
--- app/Enums/Auth/RegisterStatus.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Enums\Auth;
-
-enum RegisterStatus: string
-{
-    case SUCCESS = 'SUCCESS';
-}
