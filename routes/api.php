4de05638a2ea0746b1e1690b6bf8a501f72af2eb
Jude mpoyo <mpoyojude0@gmail.com>
2026-03-09 02:19:45 +0200

    fix: update api route file

--- routes/api.php
+++ routes/api.php
@@ -1,31 +1,30 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\Auth\ProfileController;
+use App\Http\Controllers\Api\EnumController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::middleware(['throttle:auth'])->prefix('auth')->group(function () {
+    Route::prefix('auth')->middleware('throttle:auth')->group(function () {
         Route::post('register', [AuthController::class, 'register']);
-        Route::post('login', [AuthController::class, 'login']);
+        Route::post('login', [AuthController::class, 'login'])->middleware('throttle:login');
+        Route::post('password/email', [AuthController::class, 'forgotPassword']);
+        Route::post('password/reset', [AuthController::class, 'resetPassword'])->name('password.reset');
+        Route::post('check-email', [AuthController::class, 'checkEmail']);
+        Route::get('email/verify/{id}/{hash}', [AuthController::class, 'verifyEmail'])->name('verification.verify');
 
         Route::middleware(['auth:sanctum', 'throttle:api'])->group(function () {
             Route::post('logout', [AuthController::class, 'logout']);
             Route::post('logout-all', [AuthController::class, 'logoutAll']);
+            Route::post('refresh', [AuthController::class, 'refresh']);
             Route::get('user', [AuthController::class, 'user']);
             Route::post('email/verification-notification', [AuthController::class, 'resendVerificationEmail']);
-            Route::post('refresh', [AuthController::class, 'refresh']);
 
-            // Profile Management
-            Route::put('profile', [\App\Http\Controllers\Api\Auth\ProfileController::class, 'update']);
-            Route::put('profile/password', [\App\Http\Controllers\Api\Auth\ProfileController::class, 'updatePassword']);
+            Route::patch('profile', [ProfileController::class, 'update']);
+            Route::patch('profile', [ProfileController::class, 'changeEmail']);
+            Route::patch('profile/password', [ProfileController::class, 'updatePassword']);
         });
-
-        Route::post('password/email', [AuthController::class, 'forgotPassword']);
-        Route::post('password/reset', [AuthController::class, 'resetPassword'])->name('password.reset');
-
-        Route::get('email/verify/{id}/{hash}', [AuthController::class, 'verifyEmail'])
-            ->name('verification.verify');
-
     });
     // RBAC Examples
     Route::middleware(['auth:sanctum', 'throttle:api'])->group(function () {
@@ -36,4 +35,7 @@ Route::prefix('v1')->group(function () {
             });
         });
     });
+
+    Route::get('enums/{enum}', [EnumController::class, 'show']);
+
 });
